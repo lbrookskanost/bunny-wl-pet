eff514577727bb2a327c468fd96385a5dcdc6434
lbrookskanost <142836967+lbrookskanost@users.noreply.github.com>
2026-02-09 12:39:03 -0600

    focus on class instead of this

--- src/wayland.rs
+++ src/wayland.rs
@@ -8,28 +8,57 @@ use wayland_client::{
 //use wayland_protocols::wl_shm::clinet::wl_shm;
 use memmap2::MmapMut;
 
-struct AppData;
+struct AppData{
+	compositor: Option<wl_compositor::WlCompositor>,
+	shm: Option<wl_shm::WlShm>,
+}
 
 impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
 	fn event(
-		_state: &mut Self,
-		_: &wl_registry::WlRegistry,
+		state: &mut Self,
+		registry: &wl_registry::WlRegistry,
 		event: wl_registry::Event,
 		_: &(),
 		_: &Connection,
-		_: &QueueHandle<AppData>,
+		qh: &QueueHandle<AppData>,
 	) {
 		if let wl_registry::Event::Global { name, interface, version } = event {
-			println!("[{}] {} (v{})", name, interface, version);
+			if interface == wl_compositor::WlCompositor::interface().name {
+				state.compositor = Some(
+					registry.bind(name, version, qh, ())
+				);
+			}
+			if interface == wl_shm::WlShm::interface().name {
+				state.shm = Some(
+					registry.bind(name, version, qh, ())
+				);
+			}		
 		}
 	}
 }
 pub fn run() {
+	//connects to compositor
 	let conn = Connection::connect_to_env().unwrap();
+	//creates root Wayland object, event queue
 	let display = conn.display();
 	let mut event_queue = conn.new_event_queue();
 	let qh = event_queue.handle();
+	//finds global objects
 	let _registry = display.get_registry(&qh, ());
-	println!("Advertised globals:");
-	event_queue.roundtrip(&mut AppData).unwrap();
+	let mut app = AppData {
+		compositor: None,
+		shm: None,
+	};
+	event_queue.roundtrip(&mut app).unwrap();
+	let surface = app
+		.compositor
+		.as_ref()
+		.unwrap()
+		.create_surface(&qh, ());
+	//create shm buffer
+	//attach buffer
+	//keep event loop alive
+	loop {
+		event_queue.blocking_dispatch(&mut app).unwrap();
+	}
 }

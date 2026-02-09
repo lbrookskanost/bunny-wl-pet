8275112ab5ad620a8615440639555da3081338b8
lbrookskanost <142836967+lbrookskanost@users.noreply.github.com>
2026-02-09 13:46:49 -0600

    loop working

--- src/wayland.rs
+++ src/wayland.rs
@@ -2,7 +2,7 @@ use wayland_client::{
 	Connection,
 	Dispatch,
 	QueueHandle,
-	protocol::{wl_registry, wl_compositor, wl_surface, wl_shm, wl_buffer},
+	protocol::{wl_registry, wl_compositor, wl_surface, wl_shm, wl_display, wl_buffer},
 	globals::registry_queue_init,
 };
 //use wayland_protocols::wl_shm::clinet::wl_shm;
@@ -23,19 +23,84 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
 		qh: &QueueHandle<AppData>,
 	) {
 		if let wl_registry::Event::Global { name, interface, version } = event {
-			if interface == wl_compositor::WlCompositor::interface().name {
-				state.compositor = Some(
-					registry.bind(name, version, qh, ())
-				);
-			}
-			if interface == wl_shm::WlShm::interface().name {
-				state.shm = Some(
-					registry.bind(name, version, qh, ())
-				);
+			match interface.as_str() {
+				"wl_compositor" => {
+					let compositor = registry
+						.bind::<wl_compositor::WlCompositor, _, _> (name, version, qh, ());
+					state.compositor = Some(compositor);
+				}
+				"wl_shm" => {
+					let shm = registry
+						.bind::<wl_shm::WlShm, _, _> (name, version, qh, ());
+					state.shm = Some(shm);
+				}
+				_ => {}
 			}		
 		}
 	}
 }
+
+impl Dispatch<wl_display::WlDisplay, ()> for AppData {
+	fn event(
+		_: &mut Self,
+		_: &wl_display::WlDisplay,
+		_: wl_display::Event,
+		_: &(),
+		_: &Connection,
+		_: &QueueHandle<Self>,
+	) {
+		//empty
+	}
+}
+
+impl Dispatch<wl_compositor::WlCompositor, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &wl_compositor::WlCompositor,
+        _: wl_compositor::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        // empty
+    }
+}
+
+impl Dispatch<wl_shm::WlShm, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &wl_shm::WlShm,
+        _: wl_shm::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        // sempty
+    }
+}
+
+impl Dispatch<wl_surface::WlSurface, ()> for AppData {
+    fn event(
+        _: &mut Self,
+        _: &wl_surface::WlSurface,
+        event: wl_surface::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<Self>,
+    ) {
+        match event {
+            wl_surface::Event::Enter { .. } => {
+                // enters output
+            }
+            wl_surface::Event::Leave { .. } => {
+                // leaves output
+            }
+            _ => {}
+        }
+    }
+}
+
+
 pub fn run() {
 	//connects to compositor
 	let conn = Connection::connect_to_env().unwrap();

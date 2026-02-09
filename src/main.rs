3c4c182e4f92980f61a0e97a40bc4c6e82872e2d
lbrookskanost <142836967+lbrookskanost@users.noreply.github.com>
2026-02-09 11:29:17 -0600

    add crates (fail)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod wayland;
+
 fn main() {
-    println!("Hello, world!");
+    wayland::run();
 }
---
+++ src/wayland.rs
@@ -0,0 +1,10 @@
+use wayland_client::{
+	Display, GlobalManager,
+	protocol::{wl_compositor, wlsurface},
+};
+use wayland_protocols::wl_shm::clinet::wl_shm;
+use memmap2::MmapMut;
+
+fn main() {
+	println!("Hello, world!");
+}
